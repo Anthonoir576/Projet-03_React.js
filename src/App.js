79f881515bc08d5552ca6995a0668569fd7311c5
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-11-09 16:36:58 +0100

    :FEAT: => Scroll horizontal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Switch, Route, Redirect , useLocation, useHistory } from 'react-router-dom';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import {Projet1, Projet2, Projet3, Projet4} from './pages/Portfolio';
@@ -8,9 +8,64 @@ import {Projet1, Projet2, Projet3, Projet4} from './pages/Portfolio';
 
 const App = () => {
 
+  const location = useLocation();
+  let history = useHistory();
+
+  useEffect(() => {
+    
+    const handleScrollToElement = (e) => {
+      console.log(e.wheelDeltaY);
+      const url = window.location.origin + "/";
+
+      const wheelRouter = (after, before) => {
+        if (e.wheelDeltaY < 0) {
+          setTimeout(() => {
+            history.push(after);
+          }, 500);
+        } else if (e.wheelDeltaY > 0) {
+          setTimeout(() => {
+            history.push(before);
+          }, 500);
+        }
+      };
+
+      switch (window.location.href.toString()) {
+        case url:
+          if (e.wheelDeltaY < 0) {
+            setTimeout(() => {
+              history.push("projet01");
+            }, 500);
+          }
+          break;
+        case url + "projet01":
+          wheelRouter("projet02", "");
+          break;
+        case url + "projet02":
+          wheelRouter("projet03", "projet01");
+          break;
+        case url + "projet03":
+          wheelRouter("projet04", "projet02");
+          break;
+        case url + "projet04":
+          wheelRouter("contact", "projet03");
+          break;
+        case url + "contact":
+          if (e.wheelDeltaY > 0) {
+            setTimeout(() => {
+              history.push("projet04");
+            }, 500);
+          }
+          break;
+        default:
+          console.log("nothing");
+      }
+    };
+    window.addEventListener("wheel", handleScrollToElement);
+  }, [history]);
+
   return (
 
-    <Switch>
+    <Switch location={location} key={location.pathname} >
       <Route path="/" exact component={Home} />
       <Route path="/projet01" exact component={Projet1} />
       <Route path="/projet02" exact component={Projet2} />
