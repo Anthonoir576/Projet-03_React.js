2e09899f710e4ec25808c42ac0f40091f224be9e
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-11-09 10:05:05 +0100

    :FIX: => navigation en cours fix bug nav portfolio

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,13 +8,25 @@ const Navigation = () => {
     return (
         <div className="navigation">
             <ul>
-                <NavLink to="/"
-                         exact
-                         activeClassName="nav-active"
-                >
+                <NavLink to="/" exact activeClassName="nav-active">
                     <li>Accueil</li>
                 </NavLink>
-
+                <li className="nav-portfolio">Portfolio
+                    <ul className="nav-projects">
+                        <NavLink to="/projet01" exact activeClassName="nav-active" className="hover">
+                            <li>Projet 01</li>
+                        </NavLink>
+                        <NavLink to="/projet02" exact activeClassName="nav-active" className="hover">
+                            <li>Projet 02</li>
+                        </NavLink>
+                        <NavLink to="/projet03" exact activeClassName="nav-active" className="hover">
+                            <li>Projet 03</li>
+                        </NavLink>
+                        <NavLink to="/projet04" exact activeClassName="nav-active" className="hover">
+                            <li>Projet 04</li>
+                        </NavLink>
+                    </ul>
+                </li>
 
             </ul>
         </div>
